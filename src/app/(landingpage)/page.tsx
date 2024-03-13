0cb479066dea2dda42e5611a0a48386ba8d9dcf7
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2024-03-13 17:18:36 +0700

    add signin and signup page to jobs-hunt app

--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -4,6 +4,7 @@ import Category from "@/components/organisms/Category";
 import Clients from "@/components/organisms/Clients";
 import FeaturedJobs from "@/components/organisms/FeaturedJobs";
 import Hero from "@/components/organisms/Hero";
+import LatestJobs from "@/components/organisms/LatestJobs";
 import Image from "next/image";
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
         <Category />
         <BannerSignUp />
         <FeaturedJobs />
+        <LatestJobs />
       </div>
     </>
   );
