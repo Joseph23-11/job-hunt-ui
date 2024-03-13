455f7eb4d9990b627a897054a7f8bbf3ee7e0f60
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2024-03-13 16:54:06 +0700

    application jobhuntly on page header,  footer, categories, and featureJobs

---
+++ src/app/(landingpage)/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import BannerSignUp from "@/components/organisms/BannerSignUp";
+import Category from "@/components/organisms/Category";
+import Clients from "@/components/organisms/Clients";
+import FeaturedJobs from "@/components/organisms/FeaturedJobs";
+import Hero from "@/components/organisms/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="absolute w-2/3 h-screen top-0 right-0 -z-10">
+        <Image src="/images/pattern.png" alt="/images/pattern.png" fill />
+      </div>
+      <div className="px-32 mb-10">
+        <Hero />
+        <Clients />
+        <Category />
+        <BannerSignUp />
+        <FeaturedJobs />
+      </div>
+    </>
+  );
+}
